ab2e563192d22d6365e7fcb7fb62145b9c1f086c
zy <1239930167@qq.com>
2020-01-30 23:03:32 +0800

    返回数组索引

---
+++ 33.搜索旋转排序数组.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=javascript
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] == target) {
+      return i;
+    }
+  }
+  return -1;
+};
+// @lc code=end
