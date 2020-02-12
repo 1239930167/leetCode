c538336ae3183c798c6a950704ed7ea73be90801
zy <1239930167@qq.com>
2020-02-12 22:51:24 +0800

    二叉树、杨辉三角

---
+++ 107.二叉树的层次遍历-ii.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=107 lang=javascript
+ *
+ * [107] 二叉树的层次遍历 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+var levelOrderBottom = function(root) {
+    var queue = [];
+    var result = [];
+    if (root !== null) {
+      queue.push(root);
+    }
+    while(queue.length !== 0) {
+      var level = [];
+      var len = queue.length;
+      for (var i = 0; i < len; i ++) {
+        var currentNode = queue.shift();
+        level.push(currentNode.val);
+        if (currentNode.left !== null) queue.push(currentNode.left);
+        if (currentNode.right !== null) queue.push(currentNode.right);
+      }
+      result.push(level);
+    }
+    return result.reverse();
+  }
+// @lc code=end
+
---
+++ 118.杨辉三角.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=javascript
+ *
+ * [118] 杨辉三角
+ */
+
+// @lc code=start
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let arr = new Array(numRows)
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(i + 1)
+    }
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (j == 0 || j == i) {
+                arr[i][j] = 1
+            } else {
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j]
+            }
+        }
+    }
+    return arr
+};
+// @lc code=end
+
