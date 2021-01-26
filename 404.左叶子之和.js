8f139177aa6b0a3515a66d06911bf4c43e1588e5
zy <zhouyu0318@yuantutech.com>
2021-01-26 09:25:40 +0800

    左叶子之和

---
+++ 404.左叶子之和.js
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=404 lang=javascript
+ *
+ * [404] 左叶子之和
+ *
+ * https://leetcode-cn.com/problems/sum-of-left-leaves/description/
+ *
+ * algorithms
+ * Easy (55.40%)
+ * Likes:    275
+ * Dislikes: 0
+ * Total Accepted:    67.5K
+ * Total Submissions: 119.5K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 计算给定二叉树的所有左叶子之和。
+ * 
+ * 示例：
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function (root, isLeft = false) {
+  if (!root) return 0
+  if (!root.left && !root.right && isLeft) return root.val
+  return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false)
+};
+// @lc code=end
+
