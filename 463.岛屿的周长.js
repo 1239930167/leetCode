f7a2a8c421201589f630e0fa251a424bb0d28860
zy <zhouyu0318@yuantutech.com>
2021-03-17 09:21:18 +0800

    岛屿的周长

---
+++ 463.岛屿的周长.js
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=463 lang=javascript
+ *
+ * [463] 岛屿的周长
+ *
+ * https://leetcode-cn.com/problems/island-perimeter/description/
+ *
+ * algorithms
+ * Easy (67.89%)
+ * Likes:    384
+ * Dislikes: 0
+ * Total Accepted:    63.7K
+ * Total Submissions: 89.2K
+ * Testcase Example:  '[[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]'
+ *
+ * 给定一个 row x col 的二维网格地图 grid ，其中：grid[i][j] = 1 表示陆地， grid[i][j] = 0 表示水域。
+ * 
+ * 网格中的格子 水平和垂直 方向相连（对角线方向不相连）。整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。
+ * 
+ * 岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100
+ * 。计算这个岛屿的周长。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+ * 输出：16
+ * 解释：它的周长是上面图片中的 16 个黄色的边
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：grid = [[1]]
+ * 输出：4
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：grid = [[1,0]]
+ * 输出：4
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * row == grid.length
+ * col == grid[i].length
+ * 1 
+ * grid[i][j] 为 0 或 1
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function (grid) {
+  const dx = [0, 1, 0, -1]
+  const dy = [1, 0, -1, 0]
+  const n = grid.length, m = grid[0].length
+  let ans = 0
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < m; ++j) {
+      if (grid[i][j]) {
+        let cnt = 0
+        for (let k = 0; k < 4; ++k) {
+          let tx = i + dx[k]
+          let ty = j + dy[k]
+          if (tx < 0 || tx >= n || ty < 0 || ty >= m || !grid[tx][ty]) {
+            cnt += 1
+          }
+        }
+        ans += cnt
+      }
+    }
+
+  }
+  return ans
+};
+// @lc code=end
+
